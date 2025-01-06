919a109bd94853ecd9f63ee536bc643189237edb
Cristi Tomoiaga <Cristi-Tomoiaga@users.noreply.github.com>
2025-01-06 17:38:40 +0200

    Init project

---
+++ src/main/java/ro/ubbcluj/tpjad/jadbackend/JadBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.ubbcluj.tpjad.jadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JadBackendApplication.class, args);
+    }
+
+}
