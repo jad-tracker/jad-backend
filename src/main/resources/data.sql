34d2c9f8956d5f43b3781dd40968c45ed25c277b
Cristi Tomoiaga <Cristi-Tomoiaga@users.noreply.github.com>
2025-01-16 18:03:23 +0200

    Fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,9 +10,9 @@ values ('alex', '$2a$10$mn4uqw941tuQls3upBNH8ejtIWJipcdOAErOCNPjtvnDyWYJL8Jku'),
        ('maria', '$2a$10$NiBd0CHMDpvxbPfZmZ7EBOl7qod1nR/fWUKABAQLr8FDmt44nBJ1O'), -- password: maria
        ('ioana', '$2a$10$jzZwR2GZZnmUP2HZoSZ1e.d2XE1koUcijYK8YSHEu.0ob3/fTqKWy'); -- password: ioana
 
-INSERT INTO public.projects (id, id_lead, description, name) VALUES (2, 3, 'Our cloud storage offering', 'Contoso Cloud');
-INSERT INTO public.projects (id, id_lead, description, name) VALUES (3, 1, 'Team messaging & calls', 'Congregate');
-INSERT INTO public.projects (id, id_lead, description, name) VALUES (1, 1, 'Diff checking at scale', 'Conflate');
+INSERT INTO public.projects (id_lead, description, name) VALUES (1, 'Diff checking at scale', 'Conflate');
+INSERT INTO public.projects (id_lead, description, name) VALUES (3, 'Our cloud storage offering', 'Contoso Cloud');
+INSERT INTO public.projects (id_lead, description, name) VALUES (1, 'Team messaging & calls', 'Congregate');
 
 
 insert into project_members(id_user, id_project, role)
@@ -21,12 +21,20 @@ values (2, 1, 'Dev'),
        (1, 2, 'QA'),
        (2, 2, 'Dev');
 
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2025-01-15 23:09:00.000000', 5, 1, 2, e'FREE & OPEN SOURCE
 
-# [SonarQube Community Build](https://www.sonarsource.com/open-source-editions/sonarqube-community-edition/ "Link")
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-12 18:00:00.000000', 4, 1, e'Our CI pipeline takes *way too long* to run, and some developers (not to be named) have taken to avoiding testing altogether.
 
-SonarQube Community Build provides development teams with a workflow-integrated solution to keep their codebase in a state of Clean Code. SonarQube Community Build supports dozens of popular and classic programming languages, frameworks and cloud technologies.', 'DOING', 'Integrate SonarQube into project', 'TASK');
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-15 12:00:00.000000', 4, 2, 2, e'* Obtinere proiecte proprii ale unui utilizator (inclusiv proiectele in care e membru)
+We need to improve things', 'TODO', 'DevEx - Better rigs for everyone', 'TASK');
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-13 13:00:00.000000', 2, 1, '**Conflating** things is the opposite of <u>diff</u>erentiating them.', 'DOING', 'Find a better product name', 'STORY');
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-14 09:00:00.000000', 1, 2, e'* [x] Reproduce the issue
+* [x] Find the cause
+* [ ] Fix for next release
+* [ ] Backport to 3.15
+
+
+
+The issue seems to be caused by the **WebDAV** URL being wrongly parsed. The `/dav` part gets confused with the `/david` representing the username.', 'DOING', '[Bug] Users named David can''t log in', 'BUG');
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-15 12:00:00.000000', 2, 2, e'* Obtinere proiecte proprii ale unui utilizator (inclusiv proiectele in care e membru)
   * GET /api/projects
   * Response-ul contine o lista de proiecte cu id (number), name (string), description (string), lead (string - username-ul lead-ului) - cod 200 in caz de succes
 * Creare proiect
@@ -50,25 +58,21 @@ INSERT INTO public.issues (date, id, id_assignee, id_project, description, statu
 * Stergere membru de la proiect
   * DELETE /api/projects/{projectId}/members/{memberId}
   * Response - cod 204 in caz de succes', 'DONE', 'Implement Backend', 'STORY');
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-14 09:00:00.000000', 3, 1, 2, e'* [x] Reproduce the issue
-* [x] Find the cause
-* [ ] Fix for next release
-* [ ] Backport to 3.15
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2025-01-15 23:09:00.000000', 1, 2, e'FREE & OPEN SOURCE
+
+# [SonarQube Community Build](https://www.sonarsource.com/open-source-editions/sonarqube-community-edition/ "Link")
+
+SonarQube Community Build provides development teams with a workflow-integrated solution to keep their codebase in a state of Clean Code. SonarQube Community Build supports dozens of popular and classic programming languages, frameworks and cloud technologies.', 'DOING', 'Integrate SonarQube into project', 'TASK');
 
 
-
-The issue seems to be caused by the **WebDAV** URL being wrongly parsed. The `/dav` part gets confused with the `/david` representing the username.', 'DOING', '[Bug] Users named David can''t log in', 'BUG');
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2025-01-15 23:57:00.000000', 6, 3, 2, 'There''s not much to it, we just need to show our work since last time', 'TODO', 'Prepare slide deck for VP meeting', 'TASK');
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2025-01-16 00:25:00.000000', 7, 1, 3, e'Weirdly enough, this seems related to distance. Messages only start to disappear after \\~500 miles
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2025-01-15 23:57:00.000000', 3, 2, 'There''s not much to it, we just need to show our work since last time', 'TODO', 'Prepare slide deck for VP meeting', 'TASK');
+INSERT INTO public.issues (date, id_assignee, id_project, description, status, summary, type) VALUES ('2025-01-16 00:25:00.000000', 1, 3, e'Weirdly enough, this seems related to distance. Messages only start to disappear after \\~500 miles
 
 
 
 [https://lowendbox.com/blog/we-cant-send-email-more-than-500-miles-a-true-story/](https://lowendbox.com/blog/we-cant-send-email-more-than-500-miles-a-true-story/)', 'DOING', 'Find out why some messages disappear', 'BUG');
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-12 18:00:00.000000', 1, 4, 1, e'Our CI pipeline takes *way too long* to run, and some developers (not to be named) have taken to avoiding testing altogether.
 
-We need to improve things', 'TODO', 'DevEx - Better rigs for everyone', 'TASK');
-INSERT INTO public.issues (date, id, id_assignee, id_project, description, status, summary, type) VALUES ('2024-06-13 13:00:00.000000', 2, 2, 1, '**Conflating** things is the opposite of <u>diff</u>erentiating them.', 'DOING', 'Find a better product name', 'STORY');
 
-INSERT INTO public.comments (date, id, id_issue, id_user, content) VALUES ('2025-01-15 23:54:00.000000', 3, 5, 3, 'I''ll need an update on this');
-INSERT INTO public.comments (date, id, id_issue, id_user, content) VALUES ('2025-01-16 00:01:00.000000', 4, 3, 3, 'This one''s rather unusual');
-INSERT INTO public.comments (date, id, id_issue, id_user, content) VALUES ('2024-06-13 10:00:00.000000', 1, 1, 1, 'Seems good to me');
+INSERT INTO public.comments (date, id_issue, id_user, content) VALUES ('2024-06-13 10:00:00.000000', 1, 1, 'Seems good to me');
+INSERT INTO public.comments (date, id_issue, id_user, content) VALUES ('2025-01-15 23:54:00.000000', 5, 3, 'I''ll need an update on this');
+INSERT INTO public.comments (date, id_issue, id_user, content) VALUES ('2025-01-16 00:01:00.000000', 3, 3, 'This one''s rather unusual');
